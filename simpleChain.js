bbdb870d435b6a68b4d2a41352fd9f7fad16c559
kiknaio <g.kiknadze@hotmail.com>
2018-08-10 17:37:31 +0400

    use promises and async style to manage code

--- simpleChain.js
+++ simpleChain.js
@@ -1,59 +1,82 @@
 const level = require('level');
 const SHA256 = require('crypto-js/sha256');
 const to = require('to2');
-const log = level('log.db');
+const log = level('log.db', { valueEncoding: 'json' });
 
 
 class Block {
   constructor(data) {
     this.hash = "",
-      this.height = 0,
-      this.body = data,
-      this.time = Date.now(),
-      this.previousBlockHash = ""
+    this.height = 0,
+    this.body = data,
+    this.time = Date.now(),
+    this.previousBlockHash = ""
   }
 }
 
 class Blockchain {
   constructor() {
-    this.addBlock(new Block("Genesis block"));
-  }
-
-  addBlock(newBlock) {
-	// TODO: get height
-    newBlock.height = this.getBlockchainHeight();
-    newBlock.time = new Date().getTime().toString().slice(0, -3);
-
-    if (newBlock.height > 0) {
-      newBlock.previousBlockHash = this.getBlock(newBlock.height - 1).hash;
-    }
-
-    newBlock.hash = SHA256(newBlock).toString();
-
-    log.put(newBlock.height, JSON.stringify(newBlock), err => {
-      if (err) return console.error(err);
-    });
-  }
-  
-
-  getBlockchainHeight() {
-    let height = 0;
-    log.createReadStream().on('data', () => {
-      height++;
-    });
-    return height;
-  }
-
-  // get block
-  getBlock(blockHeight) {
     let height = 0;
     log.createReadStream()
-		.pipe(to.obj((row, enc, next) => {
-      		if (blockHeight === parseInt(row.key)) {
-        		return console.log(row);
-      		}
-      		height++;
-    		}))
+      .on('data', () => height++)
+      .on('close', () => {
+        if (height === 0) {
+          console.log('Genesis block created');
+          this.addBlock(new Block("Genesis block"));
+        }
+      })
+  }
+
+  async addBlock(data) {
+    const newBlock = {};
+    let height = 0;
+
+    newBlock.time = new Date().getTime().toString().slice(0, -3);
+    newBlock.body = data;
+
+    // Get height
+    log.createReadStream()
+      .on('data', () => height++)
+      .on('close', async () => {
+        newBlock.height = height;
+
+        if (newBlock.height > 0) {
+          const previousBlock = await this.getBlock(newBlock.height - 1);
+          newBlock.previousBlockHash = previousBlock.hash;
+        }
+
+        newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
+
+        log.put(newBlock.height, newBlock, err => {
+          if (err) return console.error(err);
+          log.get(newBlock.height, (err, result) => {
+            if(err) return console.error(err);
+            console.log(result);
+          })
+        });
+      });
+  }
+
+
+  // ==================================
+  // =========== GET BLOCK ============
+  // ==================================
+  async getBlockHeight() {
+    let height = 0;
+    return new Promise((resolve, reject) => {
+      log.createReadStream()
+        .on('data', () => height++)
+        .on('end', () => resolve(height))
+    })
+  }
+
+  getBlock(blockHeight) {
+    return new Promise((resolve, reject) => {
+      log.get(blockHeight, (err, result) => {
+        if(err) return console.error(err);
+        resolve(result);
+      })
+    })
   }
 
   // validate block
@@ -97,11 +120,11 @@ class Blockchain {
   }
 
 	list() {
-		log.createReadStream()
-			.pipe(to.obj((row, enc, next) => {
-				console.log(row);
-				next();
-			}))
+    log.createReadStream()
+      .pipe(to.obj((row, enc, next) => {
+          console.log(row);
+          next();
+      }))
 	}
 }
 
@@ -112,16 +135,20 @@ const blockchain = new Blockchain();
 // TODO: Genesis block persist as the first block in the blockchain using LevelDB
 
 // TODO: addblock() save into levelDB
-// console.log(blockchain.addBlock('Test block'));
+// blockchain.addBlock('giorgi kiknadze');
 // TODO: validateBlock() function to validate a block stored within levelDB
 
 // TODO: validateChain() function to validate blockchain stored within levelDB
 
-// TODO: getBlock() function retrieves a block by block heigh within the LevelDB chain.
-// console.log(blockchain.getBlock(0));
+// GET Block
+// (async () =>
+//   console.log(await blockchain.getBlock(0)))()
 
 // TODO: getBlockHeight() function retrieves current block height within the LevelDB chain.
-// console.log(blockchain.getBlockchainHeight());
+(async () =>
+  console.log(await blockchain.getBlockHeight()))();
 
 // TODO: get list of all blocks
-blockchain.list();
+// blockchain.list();
+
+// console.log(blockchain.getBlockHeight());
